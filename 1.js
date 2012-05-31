017da6ea5bd2a70d16b3c61c686ba40bcf054e36
heiguo <heiguo@gmail.com>
2012-05-31 12:48:02 +0800

    make it work

--- 1.js
+++ 1.js
@@ -1,27 +1,66 @@
-var ps = document.getElementsByTagName("p");
-var parents = new Array();
-for (var i = 0; i < ps.length; i++) {
-  var parent = ps[i].parentNode;
-  var existed = false;
-  for (var j = 0; j < parents.length; j++) {
-    if (parents[j] == parent) {
-      existed = true;
-      break;
+var MAGIC_TEXT_LENGTH = 30;
+
+function reStyle(elem) {
+  //elem.style.fontFamily = "Corbel, Georgia, 'Microsoft Yahei'";
+  elem.style.fontSize = '18px';
+  elem.style.lineHeight = '155%';
+}
+
+function reStyleList(listElem) {
+  var lis = listElem.getElementsByTagName("li");;
+  if (lis != null) {
+    for (var i = 0; i < lis.length; i++) {
+      reStyle(lis[i]);
     }
   }
-  
-  if (existed)
-    continue;
-
-  parents.push(parent);
-  validPs = parent.getElementsByTagName("p");
-  if (validPs.length < 3)
-    continue;
-
-  for (var j = 0; j < validPs.length; j++) {
-    validPs[j].style.fontFamily = "Corbel, Georgia, 'Microsoft Yahei'";
-    validPs[j].style.fontSize = '16px';
-    validPs[j].style.lineHeight = '155%';
-  }
-  parent.style.width = '500px';
 }
+
+function processBlocks(blocks) {
+  for (var i = 0; i < blocks.length; i++) {
+    var text = blocks[i].textContent;
+    if (text.length > MAGIC_TEXT_LENGTH) {
+      reStyle(blocks[i]);
+    }
+  }  
+}
+
+function processP() {
+  var paragraphs = document.getElementsByTagName("p");
+  processBlocks(paragraphs);
+}
+
+function processPRE() {
+  var pres = document.getElementsByTagName("pre");
+  processBlocks(pres);
+}
+
+function processLists(lists) {
+  for (var i = 0; i < lists.length; i++) {
+    var list = lists[i];
+
+    //check if it's valid list
+    var lis = list.getElementsByTagName("li");
+    for (var j = 0; j < lis.length; j++) {
+      if (lis[j].textContent.length > MAGIC_TEXT_LENGTH) {
+        console.log(lis[j].textContent);
+        reStyleList(list);
+        break;
+      }
+    }
+  }
+}
+
+function processOL() {
+  var ols = document.getElementsByTagName("ol");
+  processLists(ols);
+}
+
+function processUL() {
+  var uls = document.getElementsByTagName("ul");
+  processLists(uls);
+}
+
+processP();
+processPRE();
+processOL();
+processUL();
