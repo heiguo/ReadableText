78149ab6d55554ba249924496f113c2a8f080969
heiguo <heiguo@gmail.com>
2012-06-10 14:23:38 +0800

    code --

--- 2.js
+++ 2.js
@@ -1,97 +1,85 @@
 (function() {
-  var MAGIC_TEXT_LENGTH, PREFERRED_FONT_SIZE, getElemFontSize, getLongElemInfos, getMainFontSize, getTextLength, magicZoom;
-
   var MAGIC_TEXT_LENGTH = 30;
   var PREFERRED_FONT_SIZE = 18;
 
-  var getTextLength = function(elem) {
+  // get text length of imediate child text
+  var getElemTextLength = function(elem) {
     var totalLength = 0;
-    var nodes = elem.childNodes;
-    for (var i = 0; i < nodes.length; i++) {
-      var node = nodes[i];
-      if (node.nodeType === Node.TEXT_NODE) {
-        totalLength += node.textContent.length;
+    for (var i = 0; i < elem.childNodes.length; i++) {
+      var childNode = elem.childNodes[i];
+      if (childNode.nodeType === Node.TEXT_NODE) {
+        totalLength += childNode.textContent.length;
       }
     }
     return totalLength;
   };
 
-  var getLongElemInfos = function() {
-    var longElemInfos = [];
-    var paragraphs = document.getElementsByTagName("p");
-    for (var i = 0; i < paragraphs.length; i++) {
-      var p = paragraphs[i];
-      var textLen = getTextLength(p);
-      if (textLen > MAGIC_TEXT_LENGTH) {
-        longElemInfos.push({
-          elem: p,
-          textLength: textLen
-        });
-      }
+  // util function: get font size of an element
+  var getElemFontSize = function(elem) {
+    if (document.defaultView && document.defaultView.getComputedStyle) {
+      return document.defaultView.getComputedStyle(elem, null).getPropertyValue("font-size");
+    } else {
+      return elem.style["fontSize"];
     }
-    console.log(longElemInfos);
-    return longElemInfos;
   };
 
-  // util function: get font size of an element
-  var getElemFontSize = function(el) {
-    if (document.defaultView && document.defaultView.getComputedStyle) {
-      return document.defaultView.getComputedStyle(el, null).getPropertyValue("font-size");
-    } else {
-      return el.style["fontSize"];
-    }
-  };
-  
-  var getParentNodeInfos = function() {
-    var longElemInfos = getLongElemInfos();
+  var getLongElems = function() {
+    var paragraphs = document.getElementsByTagName("p");
     var parentNodeInfos = [];
-    for (var i = 0; i < longElemInfos.length; i++) {
-      var longElemInfo = longElemInfos[i];
-      var longElem = longElemInfo.elem;
-      var alreadyIn = false;
-      for (var j = 0; j < parentNodeInfos.length; j++) {
-        var nodeInfo = parentNodeInfos[j];
-        if (nodeInfo.node === longElem.parentNode) {
-          alreadyIn = true;
-          nodeInfo.count++;
-          nodeInfo.textLength += longElemInfo.textLength;
+    for (var i = 0; i < paragraphs.length; i++) {
+      var p = paragraphs[i], textLen = getElemTextLength(p);
+      if (textLen > MAGIC_TEXT_LENGTH) {
+        var parentNode = p.parentNode;
+        var alreadyIn = false;
+        for (var j = 0; j < parentNodeInfos.length; j++) {
+          var nodeInfo = parentNodeInfos[j];
+          if (nodeInfo.node === parentNode) {
+            alreadyIn = true;
+            nodeInfo.textLength += textLen;
+            break;
+          }
+        }
+        if (!alreadyIn) {
+          var parentNodeInfo = {
+            node: parentNode,
+            sampleChild: p,
+            textLength: textLen
+          };
+          parentNodeInfos.push(parentNodeInfo);
         }
       }
-      if (!alreadyIn) {
-        var parentNodeInfo = {
-          node: longElem.parentNode,
-          sample: longElem,
-          count: 1,
-          textLength: longElemInfo.textLength
-        };
-        parentNodeInfos.push(parentNodeInfo);
-      }
     }
-    
+    console.log(parentNodeInfos);
     return parentNodeInfos;
-  }
+  };
 
   // get the sample and its font size
   var getMainFontSize = function() {
-    var parentNodeInfos = getParentNodeInfos();
+    var longElems = getLongElems();
     var maxTextLength = 0;
-    var theSample;
-    for (var k = 0; k < parentNodeInfos.length; k++) {
-      var parentNodeInfo = parentNodeInfos[k];
-      if (parentNodeInfo.textLength > maxTextLength) {
-        maxTextLength = parentNodeInfo.textLength;
-        theSample = parentNodeInfo.sample;
+    var theSampleChild = null;
+    for (var k = 0; k < longElems.length; k++) {
+      var longElem = longElems[k];
+      if (longElem.textLength > maxTextLength) {
+        maxTextLength = longElem.textLength;
+        theSampleChild = longElem.sampleChild;
       }
     }
-    console.log(theSample);
-    return parseFloat(getElemFontSize(theSample));
+    if (theSampleChild == null)
+      return -1;
+
+    console.log(theSampleChild);
+    return parseFloat(getElemFontSize(theSampleChild));
   };
 
   // commit the zoom
   var magicZoom = function() {
     var zoom;
-    zoom = PREFERRED_FONT_SIZE / getMainFontSize();
-    return document.body.style.zoom = zoom;
+    var mainFontSize = getMainFontSize();
+    if (mainFontSize != -1) {
+      zoom = PREFERRED_FONT_SIZE / getMainFontSize();
+      document.body.style.zoom = zoom;
+    }
   };
 
   magicZoom();
